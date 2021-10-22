82b67dcc3fe43529fa25aa9474309574dbf977ff
YuDeokRin <ejrfls12@naver.com>
2021-10-22 20:57:01 +0900

    Initial commit

---
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,18 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService{
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+
+    // join에서 save을 호출하면 다형성에 의해서 MemoryMemberRepository안에 있는 save가 실행이된다 .cf memberRepository 인터페이스 아님
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
